d07f301a6164b983e4e94891c8138d8d414aff00
Shardul Seth <shardulseth@aol.com>
2012-06-16 17:43:27 +0530

    init

---
+++ external/hostap/wpa_supplicant/src/crypto/sms4.h
@@ -0,0 +1,35 @@
+/***************************************************************************
+**+----------------------------------------------------------------------+**
+**|                                ****                                  |**
+**|                                ****                                  |**
+**|                                ******o***                            |**
+**|                          ********_///_****                           |**
+**|                           ***** /_//_/ ****                          |**
+**|                            ** ** (__/ ****                           |**
+**|                                *********                             |**
+**|                                 ****                                 |**
+**|                                  ***                                 |**
+**|                                                                      |**
+**|     Copyright (c) 1998 - 2012 Texas Instruments Incorporated         |**
+**|                        ALL RIGHTS RESERVED                           |**
+**|                                                                      |**
+**| Permission is hereby granted to licensees of Texas Instruments       |**
+**| Incorporated (TI) products to use this computer program for the sole |**
+**| purpose of implementing a licensee product based on TI products.     |**
+**| No other rights to reproduce, use, or disseminate this computer      |**
+**| program, whether in part or in whole, are granted.                   |**
+**|                                                                      |**
+**| TI makes no representation or warranties with respect to the         |**
+**| performance of this computer program, and specifically disclaims     |**
+**| any responsibility for any damages, special or consequential,        |**
+**| connected with the use of this program.                              |**
+**|                                                                      |**
+**+----------------------------------------------------------------------+**/
+#ifndef SMS4_H_
+#define SMS4_H_
+
+#include "common.h"
+
+void sms4_decrypt(u8 *key, u8 *iv, u8 *cipher, u8 *clear);
+
+#endif /* SMS4_H_ */
