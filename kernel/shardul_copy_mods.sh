a1eb6e3c87eef31490280efae57457e7c0979e6f
Shardul Seth <shardulseth@aol.com>
2012-06-17 02:59:01 +0530

    2wcr patch

---
+++ kernel/shardul_copy_mods.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+versiondir="../myKernel/$(cat .version)/"
+mkdir ../myKernel
+mkdir $versiondir
+cp -u arch/arm/boot/zImage ../myKernel/
+cp -u block/sio-iosched.ko $versiondir
+cp -u drivers/cpufreq/shardul_intellidemand.ko $versiondir
+cp -u drivers/cpufreq/shardul_ondemandx.ko $versiondir
+cp -u drivers/hid/hid-apple.ko $versiondir
+cp -u drivers/hid/hid-belkin.ko $versiondir
+cp -u drivers/hid/hid-microsoft.ko $versiondir
+cp -u drivers/net/tun.ko $versiondir
+cp -u drivers/net/wireless/libra/librasdioif.ko $versiondir
+cp -u drivers/scsi/scsi_wait_scan.ko $versiondir
+cp -u fs/ext4/ext4.ko $versiondir
+cp -u fs/isofs/isofs.ko $versiondir
+cp -u fs/jbd2/jbd2.ko $versiondir
